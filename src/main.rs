41fc721641e30b55c276391f7f6aaa87354b9016
Lauri Gustafsson <luutifa@jkry.org>
2019-02-13 14:48:07 +0200

    Add automatic first character case conversion

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,28 @@ fn main() {
     for line in io::stdin().lock().lines() {
         let string = line.expect("Can't read from stdin");
         for word in string.split_whitespace() {
-            let first = word.chars().next().unwrap();
-            let lowercase: String = first.to_lowercase().collect();
+            let mut chars = word.chars();
+            let first = chars.next().unwrap().to_string();
+            let lowercase = first.to_lowercase();
+
+            // if first character is a vowel we can just print it easily
             if "aeiouyåäö".contains(lowercase.as_str()) {
                 print!("{}-hay ", word);
             } else {
-                print!("{}-{}ay ", &word[first.len_utf8()..], lowercase);
+                // get second character in case we need to transform it
+                let mut second = chars.next().unwrap().to_string();
+
+                // if the first character was uppercase...
+                if first != lowercase {
+                    // convert the second char to uppercase
+                    second = second.to_uppercase();
+                }
+
+                // print the second char, the word end, and the firstay
+                print!("{}{}-{}ay ",
+                       second,
+                       &word[(first.len() + second.len())..],
+                       lowercase);
             }
         }
         println!();
