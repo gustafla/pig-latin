8e5b7c30fc6aca20a3f7edc9a10c798501e3ed11
Lauri Gustafsson <luutifa@jkry.org>
2019-02-13 13:54:16 +0200

    Implement with slices (can panic at unicode input)

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::io::{self, BufRead};
 fn main() {
     for line in io::stdin().lock().lines() {
         let string = line.expect("Can't read from stdin");
-        println!("{}", string);
+        for word in string.split_whitespace() {
+            if "aeiouy".contains(&word[..1]) {
+                print!("{}-hay ", word);
+            } else {
+                print!("{}-{}ay", &word[1..], &word[..1]);
+            }
+        }
+        println!();
     }
 }
